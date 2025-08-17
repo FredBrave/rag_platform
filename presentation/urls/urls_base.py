e949fda280f4dc71983b6740c435bf86549af9bd
FredBrave <freddy.bravo.robalino@gmail.com>
2025-08-17 17:17:22 -0500

    feat: mis_rags view created

--- presentation/urls/urls_base.py
+++ presentation/urls/urls_base.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('login/', usuario_views.loginPage, name='login'),
     path('logout/', usuario_views.logoutUser, name='logout'),
     path('', page_views.home, name='home'),
+    path('mis_rags/', page_views.mis_rags, name='mis_rags'),
 ]
--- presentation/views/page_views.py
+++ presentation/views/page_views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from infrastructure.repositories.rag_repository_django import RagRepositoryDjango
-from core.use_cases.rag_case_uses import ListarRAGsPorPrivacidad
+from core.use_cases.rag_case_uses import ListarRAGsPorPrivacidad, ListarRAGsPorUsuario
 
-@login_required
+@login_required(login_url='login')
 def home(request):
     listar_rags = ListarRAGsPorPrivacidad(RagRepositoryDjango())
     public_rags = listar_rags.execute(privacidad="publico")
@@ -12,3 +12,15 @@ def home(request):
         'public_rags': public_rags,
     }
     return render(request, 'home.html', context)
+
+
+@login_required
+def mis_rags(request):
+    listar_rags = ListarRAGsPorUsuario(RagRepositoryDjango())
+
+    user_rags = listar_rags.execute(request.user.id)
+    
+    context = {
+        "user_rags": user_rags
+    }
+    return render(request, "rags/mis_rags.html", context)
