9a328a4d8cb4fdc7269f3802c3b80cc977762452
FredBrave <freddy.bravo.robalino@gmail.com>
2025-08-24 15:47:50 -0500

    feat: added repository documento functions

--- core/repositories/documento_repository.py
+++ core/repositories/documento_repository.py
@@ -19,3 +19,13 @@ class DocumentoRepository(ABC):
     def listar(self) -> List[Documento]:
         """Lista todos los documentos."""
         pass
+
+    @abstractmethod
+    def listar_por_rag(self, rag_id: int) -> List[Documento]:
+        """Listar todos los documentos por rag"""
+        pass
+
+    @abstractmethod
+    def extraer_texto(self, documento: Documento) -> str:
+        """Extrae el texto de un documento según su tipo (pdf, docx, txt, etc.)."""
+        pass
--- infrastructure/repositories/documento_repository_django.py
+++ infrastructure/repositories/documento_repository_django.py
@@ -3,6 +3,7 @@ from core.repositories.documento_repository import DocumentoRepository
 from core.models_domain.documentos import Documento
 from infrastructure.models.documentos import Documento as DocumentoORM
 from infrastructure.models.rag import RAG
+import PyPDF2, docx
 
 class DocumentoRepositoryDjango(DocumentoRepository):
 
@@ -24,6 +25,29 @@ class DocumentoRepositoryDjango(DocumentoRepository):
     def listar(self):
         docs = DocumentoORM.objects.only("id", "nombre")
         return [Documento(id=doc.id, nombre=doc.nombre, texto_extraido=None, rag_id=doc.rag_id, archivo=doc.archivo) for doc in docs]
+    
+
+    def listar_por_rag(self, rag_id: int):
+        return Documento.objects.filter(rag_id=rag_id)
+
+    def extraer_texto(self, documento: Documento) -> str:
+        if documento.tipo == "pdf":
+            texto = ""
+            with open(documento.archivo.path, "rb") as f:
+                reader = PyPDF2.PdfReader(f)
+                for page in reader.pages:
+                    texto += page.extract_text() + "\n"
+            return texto
+
+        elif documento.tipo == "txt":
+            with open(documento.archivo.path, "r", encoding="utf-8") as f:
+                return f.read()
+
+        elif documento.tipo == "docx":
+            doc = docx.Document(documento.archivo.path)
+            return "\n".join([p.text for p in doc.paragraphs])
+
+        return ""
     
     def eliminar(self, documento_id: str) -> None:
         DocumentoORM.objects.filter(id=documento_id).delete()
